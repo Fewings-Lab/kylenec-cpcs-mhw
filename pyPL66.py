bfa9de2988b581868f9a49416c421cd2e0a90dc4
cooleyky <cooleyky@oregonstate.edu>
2020-09-08 17:16:44 -0700

    Debugging pl66 pt. 4

--- pyPL66.py
+++ pyPL66.py
@@ -11,6 +11,7 @@ import numpy as np
 import xarray as xr
 import pandas as pd
 import scipy.signal as sig
+import matplotlib.pyplot as plt
 
 def pypl66(x,dt=1,T=33):
     cutoff = T/dt
@@ -29,7 +30,7 @@ def pypl66(x,dt=1,T=33):
     j = np.arange(1,nw+1)
     t = np.pi*j
     den = (fq**2)*(t**3)
-    wts = (2*np.sin(2*fq*t)-np.sin(3*fq*t))/den
+    wts = (2*np.sin(2*fq*t)-np.sin(fq*t)-np.sin(3*fq*t))/den
 
     # make weights symmetric
     wts = np.concatenate((np.flip(wts),np.array([2*fq]),wts))
@@ -40,24 +41,41 @@ def pypl66(x,dt=1,T=33):
     cs = np.cos(t.T/nw2)
     jm = np.flip(j)
     
-    for ic in range(0, ncol-1) :
-        jgd = np.where(x[:,ic] != np.nan)
-        npts = len(jgd)
-        if npts > nw2:
-            # detrend time series, then add back trend after filtering
-            xdt = sig.detrend(x[jgd,ic])
-            trnd = x[jgd,ic]-xdt
-            y = [[cs[jm]*xdt[jm]],[xdt],[cs[j]*xdt[npts+1-j]]]
-
-            # filter
-            yf = sig.lfilter(wt, 1.0, y)
-
-            # strip off extra points
-            xf[jgd,ic] = yf[nw2+1:npts+nw2]
-
-            # add back trend
-            xf[jgd,ic] += trnd
+    for ic in range(ncol) :
+        ind = np.invert(np.isnan(x))
+        if ncol == 1:
+            npts = len(x[ind])
         else:
-            print("warning: time series is too short. ncol = "+ str(ncol))
+            npts = len(x[ind,ic])
+        if npts > nw2:
+            if ncol == 1:
+                xdt = sig.detrend(x[ind])
+                trnd = x[ind]-xdt
+                y = np.concatenate((cs[jm-1]*xdt[jm-1],xdt,cs[j-1]*xdt[npts-j]))
+    
+                # filter
+                yf = sig.lfilter(wts, 1.0, y)
+    
+                # strip off extra points
+                xf[ind] = yf[nw2:npts+nw2]
+    
+                # add back trend
+                xf[ind] += trnd
+            else:
+            # detrend time series, then add back trend after filtering
+                xdt = sig.detrend(x[ind,ic])
+                trnd = x[ind,ic]-xdt
+                y = [[cs[jm]*xdt[jm]],[xdt],[cs[j]*xdt[npts+1-j]]]
+    
+                # filter
+                yf = sig.lfilter(wts, 1.0, y)
+    
+                # strip off extra points
+                xf[ind,ic] = yf[nw2+1:npts+nw2]
+    
+                # add back trend
+                xf[ind,ic] += trnd
+        else:
+            print("warning: time series is too short. column num = "+ str(ic+1))
 
     return xf
