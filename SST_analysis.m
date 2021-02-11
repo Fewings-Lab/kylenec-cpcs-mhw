2f53307cfbaf4b1aba9593cf74f9dfcf08a87ab6
cooleyky <cooleyky@oregonstate.edu>
2021-02-10 16:33:37 -0800

    Making figures and poster slides

--- SST_analysis.m
+++ SST_analysis.m
@@ -35,28 +35,29 @@ clear
 load('ptLavapie_SST.mat')
 sst = sst-273.15; % Convert sst to degC
 ds = datestr(dn);
-SST = timeseries(sst,ds); % Make sst timeseries object
+t = datetime(ds);
+% SST = timeseries(sst,ds); % Make sst timeseries object
 % % Plot raw data
 % figure(1)
-% plot(sst)
-% title('ERA-5 SST at -35.51, -72.77')
+% plot(t,sst)
+% title('ERA-5 Sea Surface Temperature at 35.51^{\circ}S, 72.77^{\circ}W')
 % xlabel('Date')
 % datetick('x','yyyy-mmm','keeplimits','keepticks')
-% ylabel('Sea Surface Temperature [^{\circ}C]','Interpreter','tex')
+% ylabel('SST [^{\circ}C]','Interpreter','tex')
 %% Use pl66 to low-pass filter sst
 % with dt=1 (same as default) and T=168 hrs (7 days)
-sstf = pl66tn(sst,1,168);
-SSTF = timeseries(sstf,ds); % Make sstf timeseries
+sstf = pl66tn(sst,1,240);
+% SSTF = timeseries(sstf,ds); % Make sstf timeseries
 % Plot low-passed filtered data with raw data
-figure(2)
-plot(SST,'k-')
+figure(7)
+plot(t,sst,'Color',[0.5,0.5,0.5],'LineWidth',6)
 hold on
-plot(SSTF,'--')
-title('ERA-5 SST at -35.51, -72.77')
-legend('SST_{raw}','7-day low-pass filtered SST')
+plot(t,sstf,'k','LineWidth',2)
+title('ERA-5 Sea Surface Temperature at 35.51^{\circ}S, 72.77^{\circ}W','Interpreter','tex')
+legend('SST_{raw}','10-day low-pass filtered SST')
 xlabel('Date')
 datetick('x','yyyy-mmm','keeplimits','keepticks')
-ylabel('Sea Surface Temperature [^{\circ}C]','Interpreter','tex')
+ylabel('SST [^{\circ}C]','Interpreter','tex')
 
 % %% Bandpass sst
 % % 30 day low pass filter
@@ -85,7 +86,7 @@ for i=1:366*24
 end 
 % Stitch 3 instances of foo together to filter
 foo2 = cat(1,foo,foo,foo);
-foo3 = pl66tn(foo2,1,168); % apply 7-day filter
+foo3 = pl66tn(foo2,1,240); % apply 7-day filter
 
 % foo4 = pl66tn(foo3,1,(30*24)); % 30-day filter
 % % take high-pass part of signal to bandpass
@@ -93,11 +94,11 @@ foo3 = pl66tn(foo2,1,168); % apply 7-day filter
 
 sst0 = foo3(length(yd)+1:2*length(yd)); % take only the middle portion of filtered climatology
 
-SST0 = timeseries(sst0,ds); % Make corresponding timeseries
+% SST0 = timeseries(sst0,ds); % Make corresponding timeseries
 
 % Plot climatology
-figure(3)
-plot(SST0)
+figure(8)
+plot(t,sst0)
 % plot(dn(1:(365*24*2)),sst0(1:(365*24*2)))
 title('Low-pass filtered climatology of ERA-5 SST')
 xlabel('Date')
@@ -109,43 +110,48 @@ ylabel('Sea Surface Temperature [^{\circ}C]','Interpreter','tex')
 sstA = sstf-sst0;
 sig = nanstd(sstA);
 mean = nanmean(sstA);
-SSTA = timeseries(sstA,ds); % Make corresponding timeseries
+% SSTA = timeseries(sstA,ds); % Make corresponding timeseries
 
 % Plot SST'
-figure(4)
-plot(SSTA)
-title("SST' at -35.51, -72.77")
+figure(9)
+plot(t,sstA)
+title("10-day Low-pass Filtered Sea Surface Temperature Anomaly at 35.51^{\circ}S, 72.77^{\circ}W",'Interpreter','tex')
 xlabel('Date')
 datetick('x','yyyy-mmm','keeplimits','keepticks')
 hold on
-yline(mean,'--k')
-yline(mean+2*sig,'--r')
-yline(mean-2*sig,'--r')
-ylabel('Sea Surface Temperature [^{\circ}C]','Interpreter','tex')
-legend("SST'","\mu_{SST'}","\pm 2 \sigma_{SST'}")
+yline(mean+2*sig,'--','Color',[0.00,0.45,0.74])
+yline(mean-2*sig,'--','Color',[0.00,0.45,0.74])
+yline(mean,'-k')
+ylabel("SST' [^{\circ}C]",'Interpreter','tex')
+legend("SST'","\pm 2 \sigma_{SST'}")
 
 %% First-order approximation of dSST'/dt
 dsstdt = sstA(2:end)-sstA(1:end-1); 
 dsstdtf = pl66tn(dsstdt,1,(3*7*24));
-DSSTDT = timeseries(dsstdtf,ds(1:end-1,:)); % Make corresponding timeseries
+dsstdtf = dsstdtf*24; % Convert to change to degC/day from degC/hr
+% DSSTDT = timeseries(dsstdtf,ds(1:end-1,:)); % Make corresponding timeseries
 
 % Plot of SST' and dt in same figure with 2 y-axes
-figure(5)
+figure(10)
 
 yyaxis left
-plot(SSTA)
-title("$\textsf{SST' and }\frac{\partial \textsf{SST'}}{\partial \textsf{t}}\textsf{ at -35.51, -72.77}$",'Interpreter','latex')
+plot(t,sstA)
+title("$\textsf{10-day Low-pass Filtered SST' and }\frac{\partial \textsf{SST'}}{\partial \textsf{t}}\textsf{ at 35.51}^{\circ}\textsf{S, 72.77}^{\circ}\textsf{W}$",'Interpreter','latex')
 hold on
-yline(0,'--')
+yline(0,'-k')
 xlabel('Date')
 datetick('x','yyyy-mmm','keeplimits','keepticks')
-yline(mean+2*sig,'--r')
-yline(mean-2*sig,'--r')
+yline(mean+2*sig,'--','Color',[0.00,0.45,0.74])
+yline(mean-2*sig,'--','Color',[0.00,0.45,0.74])
 ylabel("SST' [^{\circ}C]",'Interpreter','tex')
 
 yyaxis right
-plot(DSSTDT)
+plot(t(1:end-1),dsstdtf)
 hold on
-yline(0,'--')
-ylabel("$\frac{\partial \textsf{SST'}}{\partial \textsf{t}} \textsf{[}^{\circ}\textsf{C/hr]}$",'Interpreter','latex')
+yline(0,'-k')
+ylabel("$\frac{\partial \textsf{SST'}}{\partial \textsf{t}} \textsf{[}^{\circ}\textsf{C/day]}$",'Interpreter','latex')
 datetick('x','yyyy-mmm','keeplimits','keepticks')
+
+% Limits for IOVWST poster figures
+xlim(datetime(["2015-Jan-01" "2017-Jan-01"]))
+datetick('x','yyyy-mmm','keeplimits')
